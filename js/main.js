e584dea483fb89dc4f7a85f2e3a4071eacf8d3fa
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2024-03-25 23:59:19 +0600

    js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var one = 10;
+var two = 20;
+var three = 30;
+var four = 40;
+var five = 50;
+var six = 60;
+var seven = 70;
+var eight = 80;
+var nine = 90;
+var ten = 100;
+
+var sum = one + two;
+var subscription = ten - four;
+var multiplication = five * six;
+var division = seven / eight;
+
+
+console.log("First result : " , sum);
+console.log("Secound result : " , subscription);
+console.log("Third Result : " , multiplication);
+console.log("fourth Result : " , division);
