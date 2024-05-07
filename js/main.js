ad6ecc40ae2dd41c2d2fd8aabd5c5f2ae447dcb5
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2024-05-07 22:00:08 +0600

    update

--- js/main.js
+++ js/main.js
@@ -1,61 +1,6 @@
-let one = 10;
-let two = 20;
-let three = 30;
-let four = 40;
-let five = 50;
-let six = 60;
-let seven = 70;
-let eight = 80;
-let nine = 90;
-let ten = 100;
-five++;
-ten--;
+const nav = document.createElement("nav");
+const div = document.createElement("div");
+const img = document.createElement("img");
+const logo = document.createElement("logo");
 
-let sum = one + two;
-let subscription = ten - four;
-let multiplication = five * six;
-let division = seven / eight;
-
-
-console.log("First result : " , sum);  // additiioin 
-console.log("Secound result : " , subscription);  // বিয়োগ
-console.log("Third Result : " , multiplication); // গুন
-console.log("fourth Result : " , division);     // ভাগ
-
-console.log("the reselts is : " , two ** 2)  // পাওয়ার 
-console.log("the reselts is : " , eight % two)  // ভাগশেষ
-console.log("the incriment reselts is : " , five ++ )  // বৃদ্ধি
-console.log("the Decrement reselts is : " , ten -- )  // হ্রাস
-
-
-// data taypes
-
-// number type 
-let a = 50;
-console.log(typeof a);
-
-// string type 
-let b = "sifat";
-console.log(typeof b);
-
-// Bigint 
-let c = 12000000.505^255;
-console.log(typeof c);
-
-// Boolean 
-let d = true;
-let e = false;
-console.log(typeof d);
-
-// undefined 
-let f ;
-console.log(typeof f);
-
-// null 
-let name = null;
-console.log(typeof name);
-
-// // Symbol
-// let Symbol =  ;
-// console.log(typeof Symbol)
-
+console.log(nav , div , img , logo);
